9dfb87fc2fedde8575694bf5ca35e765a6a3d03b
kamaron1730 <63790013+kamaron1730@users.noreply.github.com>
2022-05-10 21:58:14 +0800

    Update train.py

--- GoogLeNet/train.py
+++ GoogLeNet/train.py
@@ -1,32 +1,24 @@
+import os
+import sys
+import json
+
 import torch
 import torch.nn as nn
-import torchvision
-from torchvision import transforms
-# import torchvision.models.resnet
+from torchvision import transforms, datasets
+import torch.optim as optim
+from tqdm import tqdm
+from GoogLeNet.utils import plot_class_preds
 from GoogLeNet.model import GoogLeNet
-from torch.utils.data import DataLoader
-import math
-import torch.optim.lr_scheduler as lr_scheduler
 from torch.utils.tensorboard import SummaryWriter
-import os
-from GoogLeNet.utils import train_one_epoch, evaluate, plot_class_preds
-import argparse
 
-def main(args):
-   
-    # 图片大小
-    resize = 224
-    # 初始化验证机的最小误差为正无穷
-    best_acc = 0.0
 
-    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
-    print(args)
-    print('Start Tensorboard with "tensorboard --logdir=runs", view at http://localhost:6006/')
-    # 实例化SummaryWriter对象
-    tb_writer = SummaryWriter(log_dir="runs/weather_experiment")
-    
-    if os.path.exists("./weights") is False:
-        os.makedirs("./weights")
+def main():
+    epochs = 100
+    resize=224
+    writer = SummaryWriter(log_dir="runs/weather_experiment")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+
     data_transform = {
         "train": transforms.Compose([transforms.RandomResizedCrop(resize),
                                      transforms.RandomHorizontalFlip(),
@@ -37,100 +29,98 @@ def main(args):
                                    transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
 
-    trainloader = torchvision.datasets.ImageFolder(root="../data_set/train",
+    train_dataset = datasets.ImageFolder(root="../data_set/train",
                                                    transform=data_transform["train"])
-    valloader = torchvision.datasets.ImageFolder(root="../data_set/val",
+    validate_dataset = datasets.ImageFolder(root="../data_set/val",
                                                  transform=data_transform["val"])
-    batch_size = args.batch_size
-    
-    train_set = DataLoader(trainloader, batch_size=batch_size, shuffle=True, num_workers=0)
-    val_set = DataLoader(valloader, batch_size=batch_size, shuffle=False, num_workers=0)
-    net =GoogLeNet(num_classes=args.num_classes,aux_logits=True,init_weights=True).to(device)
-    # 将模型写入tensorboard
-    init_img = torch.zeros((1, 3, 224, 224), device=device)
-    tb_writer.add_graph(net, init_img)
-    # 如果存在预训练权重则载入
-    if os.path.exists(args.weights):
-        weights_dict = torch.load(args.weights, map_location=device)
-        load_weights_dict = {k: v for k, v in weights_dict.items()
-                             if net.state_dict()[k].numel() == v.numel()}
-        net.load_state_dict(load_weights_dict, strict=False)
-    else:
-        print("not using pretrain-weights.")
+    train_num = len(train_dataset)
 
-    # 是否冻结权重
-    if args.freeze_layers:
-        print("freeze layers except fc layer.")
-        for name, para in net.named_parameters():
-            # 除最后的全连接层外，其他权重全部冻结
-            if "fc" not in name:
-                para.requires_grad_(False)
-    
-    # 损失函数为交叉熵损失函数
+    weather_list = train_dataset.class_to_idx
+    cla_dict = dict((val, key) for key, val in weather_list.items())
+    # write dict into json file
+    json_str = json.dumps(cla_dict, indent=4)
+    with open('class_indices.json', 'w') as json_file:
+        json_file.write(json_str)
 
-    criterion = nn.CrossEntropyLoss()
+    batch_size = 16
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
 
-    # 定义优化器（随机梯度下降SGD优化器，学习率为0.001）
-    pg = [p for p in net.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(net.parameters(), lr=args.lr)
-    lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
-    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
-    for epoch in range(args.epochs):
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=0)
+    val_num = len(validate_dataset)
+    validate_loader = torch.utils.data.DataLoader(validate_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=0)
+
+    print("using {} images for training, {} images for validation.".format(train_num,
+                                                                           val_num))
+
+ 
+    net = GoogLeNet(num_classes=6, aux_logits=True, init_weights=True)
+    net.to(device)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.0003)
+
+
+    best_acc = 0.0
+    if os.path.exists("./weights") is False:
+        os.makedirs("./weights")
+    save_path = './weights/googleNet.pth'
+    train_steps = len(train_loader)
+    for epoch in range(epochs):
         # train
-        mean_loss = train_one_epoch(model=net,
-                                    optimizer=optimizer,
-                                    data_loader=train_set,
-                                    device=device,
-                                    epoch=epoch)
-        # update learning rate
-        scheduler.step()
+        net.train()
+        running_loss = 0.0
+        #train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, data in enumerate(train_loader):
+            images, labels = data
+            optimizer.zero_grad()
+            logits, aux_logits2, aux_logits1 = net(images.to(device))
+            loss0 = loss_function(logits, labels.to(device))
+            loss1 = loss_function(aux_logits1, labels.to(device))
+            loss2 = loss_function(aux_logits2, labels.to(device))
+            loss = loss0 + loss1 * 0.3 + loss2 * 0.3
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+
 
         # validate
-        acc = evaluate(model=net,
-                       data_loader=val_set,
-                       device=device)
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            #val_bar = tqdm(validate_loader, file=sys.stdout)
+            for val_data in validate_loader:
+                val_images, val_labels = val_data
+                outputs = net(val_images.to(device))  # eval model only have last output layer
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
 
-        # add loss, acc and lr into tensorboard
-        print("[epoch {}] accuracy: {}".format(epoch+1, round(acc, 3)))
-        tags = ["train_loss", "accuracy", "learning_rate"]
-        tb_writer.add_scalar(tags[0], mean_loss, epoch+1)
-        tb_writer.add_scalar(tags[1], acc, epoch+1)
-        tb_writer.add_scalar(tags[2], optimizer.param_groups[0]["lr"], epoch+1)
-        # add figure into tensorboard
+        val_accurate = acc / val_num
+        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
+              (epoch + 1, running_loss / train_steps, val_accurate))
+        writer.add_scalar("train_loss",running_loss / train_steps)
+        writer.add_scalar("accuracy",val_accurate)
+        writer.flush()
         fig = plot_class_preds(net=net,
-                               images_dir="../data_random",
+                               images_dir="data_random",
                                transform=data_transform["val"],
-                               num_plot=5,
+                               num_plot=8,
                                device=device)
         if fig is not None:
-            tb_writer.add_figure("predictions vs. actuals",
+            writer.add_figure("predictions vs. actuals",
                                  figure=fig,
-                                 global_step=epoch+1)
+                                 global_step=epoch)
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
 
-      
-        # save weights
-        if acc > best_acc:
-            best_acc = acc
-            torch.save(net.state_dict(), "./weights/modelGoogLeNet.pth")
+    print('Finished Training')
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--num_classes', type=int, default=6)
-    parser.add_argument('--epochs', type=int, default=5)
-    parser.add_argument('--batch-size', type=int, default=16)
-    parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--lrf', type=float, default=0.1)
-
-    # 数据集所在根目录
-    
-    img_root = "../data_set/weather_classification"
-    parser.add_argument('--data-path', type=str, default=img_root)
-
-    parser.add_argument('--weights', type=str, default='../model/googlenet-pre.pth',
-                        help='initial weights path')
-    parser.add_argument('--freeze-layers', type=bool, default=True)
-    parser.add_argument('--device', default='cuda', help='device id (i.e. 0 or 0,1 or cpu)')
-
-    opt = parser.parse_args()
-
-    main(opt)
+    main()
